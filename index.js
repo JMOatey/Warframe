69e42da8a811cd1ac316967af7bcff313dfb593c
John Oatey <John.Oatey@veteransunited.com>
2020-05-01 19:49:53 -0400

    got colors to work

--- index.js
+++ index.js
@@ -30,32 +30,41 @@ var list = [
     'Skins',
 ];
 
+const grey = '111111';
+
 var workbook = new Excel.Workbook();
 list.forEach(item => {
     var number = 0;
-    var row = 0;
+    var row = 2;
     var sheet = workbook.addWorksheet(item);
     category = new Items({category: [item]});
     sheet.columns = [
-        { header: 'Have it?', key: 'owned', width: 20 },
+        { header: 'Have it?', key: 'owned', width: 15 },
+        { header: 'Mastered?', key: 'mastery', width: 15},
         { header: 'Name', key: 'name', width: 20 },
         { header: 'AmountRequired', key: 'required', width: 20},
         // { header: 'Locations', key: 'locations', width: 50}
       ];
     category.forEach(thing => {
-        if(number < 1){
-            console.log('thing: ', thing);
-            number = 1;
-        }
+        // if(number < 1){
+        //     console.log('thing: ', thing);
+        //     number = 1;
+        // }
             
-        sheet.addRow({owned: false, name: thing.name, required: 0});
+        sheet.addRow({owned: false, mastery: false, name: thing.name, required: 0});
+
         if(thing.components) {
+            sheet.getRow(row).fill = {
+                type: 'pattern',
+                pattern: 'solid',
+                fgColor: {argb: 'FF00FF00'}
+            };
             thing.components.forEach(component => {
-                if(number < 2)
-                {
-                    console.log('Component: ', component);
-                    number = 2;
-                }
+                // if(number < 2)
+                // {
+                //     console.log('Component: ', component);
+                //     number = 2;
+                // }
                 // var description;
                 // if(component.drops) {
                 //     description = component.drops.filter(dropLocale => dropLocale.location);
@@ -65,9 +74,11 @@ list.forEach(item => {
                 //     }
                 // }
                 // var locations = component.drops.map(dropLocale => dropLocale.location);
-                sheet.addRow({owned: false, name: component.name, required: component.itemCount});
+                sheet.addRow({owned: false, mastery: false, name: component.name, required: component.itemCount});
+                row++;
             });
         }
+        row++;
     });
 });
 
